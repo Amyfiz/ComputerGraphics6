a39495f9f837de3377697627964aff42564222e9
KreKer_ <platon.bob@gmail.com>
2024-11-04 12:56:33 +0300

    added figure selection
    can now select between figures (all cubes)

--- WindowsFormsApp1/WindowsFormsApp1/Edge.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Edge.cs
@@ -8,9 +8,15 @@ namespace WindowsFormsApp1
 {
     public class Edge
     {
-        Point a { get; set; }
-        Point b { get; set; }
+        public Point start { get; set; }
+        public Point end { get; set; }
         List<Point> neighbors;
 
+
+        public Edge(Point _a, Point _b)
+        {
+            start = _a;
+            end = _b;
+        }
     }
 }
